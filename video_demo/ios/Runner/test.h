627e3a345f95262641499ed068ce12704d5f8028
River <251475337@qq.com>
2020-03-18 18:29:30 +0800

    阿里视频环境配置

---
+++ video_demo/ios/Runner/test.h
@@ -0,0 +1,17 @@
+//
+//  test.h
+//  Runner
+//
+//  Created by River on 2020/3/18.
+//  Copyright © 2020 The Chromium Authors. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ video_demo/ios/Runner/test.mm
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  Runner
+//
+//  Created by River on 2020/3/18.
+//  Copyright © 2020 The Chromium Authors. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
